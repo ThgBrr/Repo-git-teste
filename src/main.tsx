48699a0ea550ddd6f6626c5e9c53974eb5b2188c
Thiago Barreiros <139813105+ThgBrr@users.noreply.github.com>
2026-03-16 20:46:33 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app' // Importando o arquivo app.tsx (minúsculo)
+import App from './app' // Certifique-se de que o arquivo src/app.tsx existe
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
