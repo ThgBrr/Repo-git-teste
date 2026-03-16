feec71a70c93c97ee5b1a87a1a394d73e267d65a
Thiago Barreiros <139813105+ThgBrr@users.noreply.github.com>
2026-03-16 20:38:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Repo-git-teste/', // O nome EXATO do seu repositório no GitHub
+  base: '/Repo-git-teste/', // O nome do seu repositório entre barras
 })
